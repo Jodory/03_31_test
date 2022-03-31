85fadb440d776760c33681a45e076d47ec9919d7
Jodory <jodory9524@gmail.com>
2022-03-31 14:23:20 +0900

    수업중에수정

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-const numbers = [1, 2, 3, 4, 5]
-const fruits = ["Apple", "Banana", "Cherry"]
-
-numbers.forEach(function(item, i) {
-    console.log(`${item} : ${i+1}번째 숫자`)
-})
-
-fruits.forEach(function(item, i) {
-    console.log(`${item} : ${i+1}번째 과일`)
-})
-
-console.log(fruits)
+console.log("JavaScripts")
